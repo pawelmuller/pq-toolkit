0ee49c93e99ca75d980e77252ef301cc4980346a
Paweł Müller <pawelmuller42@gmail.com>
2024-01-05 17:14:48 +0100

    Woops, that shouldn't be here

--- src/pq-toolkit/src/api_client/exceptions.py
+++ src/pq-toolkit/src/api_client/exceptions.py
@@ -2,9 +2,3 @@ class PqToolkitException(Exception):
     def __init__(self, message="An exception occurred"):
         self.message = message
         super().__init__(self.message)
-
-
-class PqToolkitNoTypeToReturnException(Exception):
-    def __init__(self, message="The "):
-        self.message = message
-        super().__init__(self.message)
