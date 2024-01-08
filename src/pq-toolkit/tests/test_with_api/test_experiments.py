7d7a24ceed0d1988d9340c767e68f9a718ca01a3
Paweł Müller <pawelmuller42@gmail.com>
2024-01-08 17:31:50 +0100

    Added tests using API

---
+++ src/pq-toolkit/tests/test_with_api/test_experiments.py
@@ -0,0 +1,39 @@
+import unittest
+
+from api_client import PqToolkitAPIClient
+from test_utils import generate_random_experiment_name
+
+
+class TestExperiments(unittest.TestCase):
+    def setUp(self):
+        self.client = PqToolkitAPIClient()
+        self.experiments_to_remove = []
+
+    def tearDown(self):
+        for experiment in self.experiments_to_remove:
+            self.client.delete_experiment(experiment_name=experiment)
+
+        api_experiments = self.client.get_experiments()
+        for experiment in self.experiments_to_remove:
+            self.assertNotIn(experiment, api_experiments)
+            self.experiments_to_remove.remove(experiment)
+
+    def test_create_experiment(self):
+        experiment_name = generate_random_experiment_name()
+        experiments = self.client.create_experiment(experiment_name=experiment_name)
+        self.experiments_to_remove.append(experiment_name)
+        self.assertIn(experiment_name, experiments)
+
+    def test_delete_experiment(self):
+        experiment_name = generate_random_experiment_name()
+        experiments = self.client.create_experiment(experiment_name=experiment_name)
+        self.experiments_to_remove.append(experiment_name)
+        self.assertIn(experiment_name, experiments)
+
+        experiments = self.client.delete_experiment(experiment_name=experiment_name)
+        self.experiments_to_remove.append(experiment_name)
+        self.assertNotIn(experiment_name, experiments)
+
+
+if __name__ == '__main__':
+    unittest.main()
