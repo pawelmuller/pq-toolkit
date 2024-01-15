c583b05a8125c0548fe3d3d5db5253c50c55c588
Bartlomiej Piktel <b.piktel@stud.elka.pw.edu.pl>
2024-01-15 22:40:22 +0100

    Fix failing test

--- src/pq-toolkit-ui/src/lib/components/player/__tests__/MultiPlayer.test.tsx
+++ src/pq-toolkit-ui/src/lib/components/player/__tests__/MultiPlayer.test.tsx
@@ -4,14 +4,16 @@ import MultiPlayer from '../MultiPlayer'
 
 describe('MultiPlayer', () => {
   it('renders a single sample player', () => {
+    const [state, setState] = [0, jest.fn()]
     render(
       <MultiPlayer
         assets={
-          new Map<string, string>([
-            ['sample1', 'mock.mp3'],
-            ['sample2', 'mock2.mp3']
+          new Map<string, { url: string; footers?: JSX.Element[] }>([
+            ['sample1', { url: 'mock.mp3' }],
+            ['sample2', { url: 'mock2.mp3' }]
           ])
         }
+        selectedPlayerState={[state, setState]}
       />
     )
 
--- src/pq-toolkit-ui/src/lib/components/player/__tests__/SinglePlayer.test.tsx
+++ src/pq-toolkit-ui/src/lib/components/player/__tests__/SinglePlayer.test.tsx
@@ -1,4 +1,3 @@
-/* eslint-disable @typescript-eslint/no-non-null-assertion */
 import '@testing-library/jest-dom'
 import { render, screen } from '@testing-library/react'
 
