e2bd80cfebb7f0a8f5af362463751b4431eb9aa4
mikolajolczak <mikolaj.olczak12@gmail.com>
2024-03-20 22:37:52 +0100

    added login form to admin page

---
+++ src/pq-toolkit-ui/src/app/api/v1/login/route.ts
@@ -0,0 +1,73 @@
+import { NextResponse } from "next/server";
+import { cookies } from "next/headers";
+import { getJwtSecretKey } from "@/lib/authentication/get-jwt";
+import { SignJWT } from "jose";
+
+// Create a POST request handler
+export async function POST(request: Request) {
+  // Read the password from the request body
+  const data = await request.json();
+  const password = data.password;
+
+  // Read the admin secret from the .env.local file
+  const adminSecret = process.env.NEXT_PUBLIC_ADMIN_SECRET;
+
+  // Check that the password is correct
+  try {
+    if (password === adminSecret) {
+      // Create a signed JWT token
+      const token = await new SignJWT({
+        password: password,
+        role: "admin", // Set your own roles
+      })
+        .setProtectedHeader({ alg: "HS256" })
+        .setIssuedAt()
+        .sign(getJwtSecretKey());
+
+      // Set a cookie with the signed JWT token
+      cookies().set({
+        name: "auth",
+        value: token,
+        secure: true,
+        sameSite: "none",
+      });
+
+      // Redirect the user to the home page
+      return NextResponse.json(`Correct pass`, {
+        // a 200 status is required to redirect from a POST to a GET route
+        status: 200,
+      });
+    } else {
+      // Redirect the user back to the login page if the password is incorrect
+      return NextResponse.json(`Could not authenticate user`, {
+        // a 200 status is required to redirect from a POST to a GET route
+        status: 401,
+      });
+    }
+  } catch (error) {
+    // Redirect the user back to the login page if there was an error
+    console.error("Error:", error);
+    return NextResponse.json(`server error`, {
+      // a 500 status is required to redirect from a POST to a GET route
+      status: 500,
+    });
+  }
+}
+
+export async function GET(request: Request) {
+
+  // Check that the password is correct
+  try {
+    if (cookies().has('auth')) {
+      return NextResponse.json('Authorized',{status:200})
+    }
+      return NextResponse.json('Unauthorized',{status:200})
+  } catch (error) {
+    // Redirect the user back to the login page if there was an error
+    console.error("Error:", error);
+    return NextResponse.json(`server error`, {
+      // a 500 status is required to redirect from a POST to a GET route
+      status: 500,
+    });
+  }
+}
