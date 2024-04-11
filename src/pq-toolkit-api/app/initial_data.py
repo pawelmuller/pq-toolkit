b5567bfb3e0826b20621623b98aeaedac9127f3b
Igor Matynia <igor.matynia@gmail.com>
2024-04-11 16:05:00 +0200

    Added database setup files

---
+++ src/pq-toolkit-api/app/initial_data.py
@@ -0,0 +1,22 @@
+import logging
+
+from sqlalchemy.orm import Session
+from app.core.db import engine, init_db
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+
+def init() -> None:
+    with Session(engine) as session:
+        init_db(session)
+
+
+def main() -> None:
+    logger.info("Creating initial data")
+    init()
+    logger.info("Initial data created")
+
+
+if __name__ == "__main__":
+    main()
