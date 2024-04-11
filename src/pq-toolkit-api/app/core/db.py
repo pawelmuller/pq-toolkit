7c54d1117a6988bfd2fd0727ee85b4e04d01d994
JakubWozniak <JakubWozniak2211@hotmail.com>
2024-04-11 22:28:37 +0200

    added db setup

--- src/pq-toolkit-api/app/core/db.py
+++ src/pq-toolkit-api/app/core/db.py
@@ -1,12 +1,13 @@
 from sqlalchemy import create_engine
-from sqlalchemy.orm import Session
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
 
 from app import crud
 from app.core.config import settings
 # from app.models import TODO
 
+
 engine = create_engine(str(settings.SQLALCHEMY_DATABASE_URI))
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+Base = declarative_base()
 
-
-def init_db(session: Session) -> None:
-    pass  # TODO: Default values to insert into the DB
