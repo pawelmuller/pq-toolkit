7e7b7348313a531ff5097b1c52e5af1224ce507f
Igor Matynia <igor.matynia@gmail.com>
2024-04-12 17:44:17 +0200

    samples are saved to subfolders, added listing

--- src/pq-toolkit-api/app/core/sample_manager.py
+++ src/pq-toolkit-api/app/core/sample_manager.py
@@ -1,12 +1,28 @@
 from minio import Minio
+import minio
+import minio.datatypes
 from pydantic import AnyUrl
-from io import FileIO
+from io import IOBase
 from urllib3.response import HTTPResponse
 from pydantic_settings import BaseSettings
+from collections import deque
 
 
 class SampleManager:
-    def __init__(self, endpoint: AnyUrl, port: int, access_key: str, secret_key: str, sample_bucket_name: str = "samples") -> None:
+    """Wrapper class for a MinIO client. Manages sample recording files.
+    """
+    _SEPARATOR = "/"
+
+    def __init__(self, endpoint: str, port: int, access_key: str, secret_key: str, sample_bucket_name: str = "samples") -> None:
+        """Creates manager object
+
+        Args:
+            endpoint (str): s3 compatible bucket service endpoint (for example MinIO image)
+            port (int): bucket service port
+            access_key (str): service access key / username
+            secret_key (str): service secret key / password
+            sample_bucket_name (str, optional): Bucket name to store samples in. Defaults to "samples".
+        """
         self._sample_bucket_name = sample_bucket_name
         self._client = Minio(
             endpoint=f"{endpoint}:{port}",
@@ -25,20 +41,23 @@ class SampleManager:
             secret_key=settings.MINIO_ROOT_PASSWORD,
         )
 
-    def _object_name_from_experiment_and_sample(experiment_name: str, sample_name: str):
-        return f"{experiment_name}/{sample_name}"
+    def _object_name_from_experiment_and_sample(self, experiment_name: str, sample_name: str):
+        if self._SEPARATOR in experiment_name or self._SEPARATOR in sample_name:
+            raise Exception(
+                f"Experiment and sample name cannot conain {self._SEPARATOR}")
+        return f"{experiment_name}{self._SEPARATOR}{sample_name}"
 
     def _ensure_bucket_exists(self):
         if not self._client.bucket_exists(self._sample_bucket_name):
             self._client.make_bucket(self._sample_bucket_name)
 
-    def upload_sample(self, experiment_name: str, sample_name: str, sample_data: FileIO):
+    def upload_sample(self, experiment_name: str, sample_name: str, sample_data: IOBase):
         object_name = self._object_name_from_experiment_and_sample(
             experiment_name, sample_name)
         self._client.put_object(self._sample_bucket_name,
                                 object_name, sample_data, length=-1, part_size=10*1024*1024)
 
-    def get_sample(self, experiment_name: str, sample_name: str):
+    def get_sample(self, experiment_name: str, sample_name: str) -> bytes:
         try:
             object_name = self._object_name_from_experiment_and_sample(
                 experiment_name, sample_name)
@@ -55,5 +74,18 @@ class SampleManager:
             experiment_name, sample_name)
         self._client.remove_object(self._sample_bucket_name, object_name)
 
-    def list_matching_samples(self, experiment_name: str):
-        return list(self._client.list_objects(prefix=experiment_name))
+    def list_matching_samples(self, experiment_name: str) -> list[str]:
+        sample_names = []
+
+        bucket_objects_iterator = self._client.list_objects(
+            bucket_name=self._sample_bucket_name,
+            prefix=experiment_name + self._SEPARATOR)
+
+        for obj in bucket_objects_iterator:
+            obj: minio.datatypes.Object
+            experiment_name, sample_name = obj.object_name.split("/")
+            sample_names.append(sample_name)
+        return sample_names
+
+    def remove_all_samples(self):
+        self._client.remove_bucket(self._sample_bucket_name)
