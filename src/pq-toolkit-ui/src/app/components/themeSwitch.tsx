46e3689823d5d83767188d256d39cd4bb7f6704b
Nokerr21 <noker3009@gmail.com>
2024-05-15 20:43:47 +0200

    fix: theme change button loading

--- src/pq-toolkit-ui/src/app/components/themeSwitch.tsx
+++ src/pq-toolkit-ui/src/app/components/themeSwitch.tsx
@@ -11,24 +11,23 @@ export default function ThemeSwitch() {
 
   useEffect(() =>  setMounted(true), [])
 
-  if (!mounted) return (
-    <Image
-      src="data:image/svg+xml;base64,PHN2ZyBzdHJva2U9IiNGRkZGRkYiIGZpbGw9IiNGRkZGRkYiIHN0cm9rZS13aWR0aD0iMCIgdmlld0JveD0iMCAwIDI0IDI0IiBoZWlnaHQ9IjIwMHB4IiB3aWR0aD0iMjAwcHgiIHhtbG5zPSJodHRwOi8vd3d3LnczLm9yZy8yMDAwL3N2ZyI+PHJlY3Qgd2lkdGg9IjIwIiBoZWlnaHQ9IjIwIiB4PSIyIiB5PSIyIiBmaWxsPSJub25lIiBzdHJva2Utd2lkdGg9IjIiIHJ4PSIyIj48L3JlY3Q+PC9zdmc+Cg=="
-      width={36}
-      height={36}
-      sizes="36x36"
-      alt="Loading Light/Dark Toggle"
-      priority={false}
-      title="Loading Light/Dark Toggle"
-    />
+  if (!mounted) 
+    return (
+      resolvedTheme === 'dark' ? 
+          <FaSun className="text-blue-500 no-underline hover:text-pink-500 dark:hover:text-pink-600 hover:text-underline 
+          transform hover:scale-105 duration-300 ease-in-out h-5 w-5 md:h-8 md:w-8 mt-1" onClick={() => setTheme('light')} /> :
+          <FaMoon className="text-blue-400 no-underline hover:text-pink-500 hover:text-underline 
+          transform hover:scale-105 duration-300 ease-in-out h-5 w-5 md:h-8 md:w-8 mt-1" onClick={() => setTheme('dark')} />
   )
 
   if (resolvedTheme === 'dark') {
-    return <FaSun className="text-blue-500 no-underline hover:text-pink-500 dark:hover:text-pink-600 hover:text-underline transform hover:scale-105 duration-300 ease-in-out h-5 w-5 md:h-8 md:w-8 mt-1" onClick={() => setTheme('light')} />
+    return <FaSun className="text-blue-500 no-underline hover:text-pink-500 dark:hover:text-pink-600 hover:text-underline 
+    transform hover:scale-105 duration-300 ease-in-out h-5 w-5 md:h-8 md:w-8 mt-1" onClick={() => setTheme('light')} />
   }
 
   if (resolvedTheme === 'light') {
-    return <FaMoon className="text-blue-400 no-underline hover:text-pink-500 hover:text-underline transform hover:scale-105 duration-300 ease-in-out h-5 w-5 md:h-8 md:w-8 mt-1" onClick={() => setTheme('dark')} />
+    return <FaMoon className="text-blue-400 no-underline hover:text-pink-500 hover:text-underline 
+    transform hover:scale-105 duration-300 ease-in-out h-5 w-5 md:h-8 md:w-8 mt-1" onClick={() => setTheme('dark')} />
   }
 
 }
