2f6bd89f9628e15a3eb904f424d340d0d7d5a264
Nokerr21 <noker3009@gmail.com>
2024-05-16 23:59:36 +0200

    add: blobs component to Home page and change configured experiments component

--- src/pq-toolkit-ui/src/app/page.tsx
+++ src/pq-toolkit-ui/src/app/page.tsx
@@ -5,22 +5,21 @@ import useSWR from 'swr'
 import Loading from './loading'
 import { validateApiData } from '@/core/apiHandlers/clientApiHandler'
 import Header from '@/lib/components/basic/header'
+import Blobs from './components/blobs'
 
 const Home = (): JSX.Element => {
   return (
-    <div className=" min-h-screen bg-gray-100 dark:bg-stone-900">
+    <div className="min-h-screen bg-gray-100 dark:bg-stone-900">
       <Header />
-        <div className="flex flex-col h-full w-full items-center justify-center my-auto fadeInUp mt-40">
-          <div className="relative text-center mb-md">
-            <div className='absolute -top-14 right-36 w-80 h-80 bg-gradient-to-r from-pink-700 to-purple-500 rounded-full mix-blend-multiply dark:mix-blend-color-dodge filter blur-xl opacity-50 animate-blob animation-delay-2000' />
-            <div className='absolute -top-14 left-36 w-80 h-80 bg-gradient-to-r from-cyan-600 to-pink-500 rounded-full mix-blend-multiply dark:mix-blend-color-dodge filter blur-xl opacity-60 animate-blob animation-delay-6000' />
-            <div className='absolute top-10 right-64 w-80 h-80 bg-gradient-to-r from-pink-500 to-pink-700 rounded-full mix-blend-multiply dark:mix-blend-color-dodge filter blur-xl opacity-50 animate-blob animation-delay-4000' />
-            <div className='absolute -top-28 -right-6 w-96 h-96 bg-gradient-to-r from-purple-500 to-violet-600 dark:from-purple-600 dark:to-violet-600 rounded-full mix-blend-multiply dark:mix-blend-color-dodge filter blur-xl opacity-60 dark:opacity-40 animate-blob animation-delay-8000' />
-            <div className='absolute -top-24 -left-11 w-96 h-96 bg-gradient-to-r from-indigo-500 to-cyan-600 rounded-full mix-blend-multiply dark:mix-blend-color-dodge filter blur-xl opacity-50 animate-blob animation-delay-10000' />
-            <h1 className="relative text-6xl font-bold">Perceptual Qualities Toolkit</h1>
-            <h2 className="relative text-xl font-semibold mt-sm">
-              Home page of experiment UI for Perceptual Qualities Python Toolkit
-            </h2>
+        <div className="flex flex-col h-full w-full items-center justify-center my-auto mt-40">
+          <div className="relative text-center mb-lg">
+            <Blobs />
+            <div className='fadeInUp'>
+              <h1 className="relative text-5xl md:text-6xl font-bold">Perceptual Qualities Toolkit</h1>
+              <h2 className="relative text-lg md:text-xl font-semibold mt-sm">
+                Home page of experiment UI for Perceptual Qualities Python Toolkit
+              </h2>
+            </div>
           </div>
           <ExperimentsListWidget />
         </div>
@@ -31,10 +30,15 @@ const Home = (): JSX.Element => {
 const ExperimentsListWidget = (): JSX.Element => {
   const { data: apiData, error, isLoading } = useSWR(`/api/v1/experiments`)
 
-  if (isLoading) return <Loading />
+  if (isLoading)
+    return (
+      <div className='fadeInUp'>
+        <Loading/>
+      </div>
+    )
   if (error != null)
     return (
-      <div className="flex w-full items-center justify-center text-center h2">
+      <div className="flex w-full fadeInUp items-center justify-center text-center h2">
         API Error
         <br />
         {error.toString()}
@@ -47,18 +51,18 @@ const ExperimentsListWidget = (): JSX.Element => {
   if (validationError != null) {
     console.error(validationError)
     return (
-      <div className="flex w-full items-center justify-center text-center h2">
+      <div className="flex w-full fadeInUp items-center justify-center text-center h2">
         Invalid data from API, please check console for details
       </div>
     )
   }
 
   return (
-    <div className="flex flex-col items-center z-10">
-      <div>Configured experiments:</div>
-      <ul>
+    <div className="flex flex-col items-center fadeInUp z-10 w-full max-w-2xl mx-auto bg-white/10 dark:bg-gray-800/10 backdrop-blur-md rounded-3xl p-8 shadow-2xl">
+      <div className='font-bold text-xl md:text-2xl mb-10'>Configured Experiments</div>
+      <ul className="space-y-2 w-full">
         {data.experiments.map((name, idx) => (
-          <li key={idx} className="text-center justify-center hover:bg-blue-400 rounded-md">
+          <li key={idx} className="text-center text-base font-semibold justify-center bg-blue-400 dark:bg-blue-500 hover:bg-pink-500 dark:hover:bg-pink-600 rounded-md p-2 transition-transform transform hover:scale-105 whitespace-normal break-words">
             <Link href={`/${name}`}>{name}</Link>
           </li>
         ))}
