9b286622e3a7755dee2de7d52f5fb743f64066ce
Nokerr21 <noker3009@gmail.com>
2024-05-16 10:55:10 +0200

    add: scroll up button to About page

--- src/pq-toolkit-ui/src/app/about/page.tsx
+++ src/pq-toolkit-ui/src/app/about/page.tsx
@@ -1,7 +1,34 @@
 'use client'
 import Header from '@/lib/components/basic/header'
+import { useEffect, useState } from 'react'
+import { FaArrowUp } from 'react-icons/fa'
 
 const About = (): JSX.Element => {
+  
+  const [isVisible, setIsVisible] = useState(false)
+
+  const toggleVisibility = () => {
+    if (window.pageYOffset > 300) {
+      setIsVisible(true)
+    } else {
+      setIsVisible(false)
+    }
+  }
+
+  const scrollToTop = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth'
+    })
+  }
+
+  useEffect(() => {
+    window.addEventListener('scroll', toggleVisibility)
+    return () => {
+      window.removeEventListener('scroll', toggleVisibility)
+    }
+  }, [])
+
   return (
   <div className="min-h-screen bg-gray-100 dark:bg-stone-900 text-black dark:text-neutral-200">
     <Header />
@@ -34,10 +61,10 @@ const About = (): JSX.Element => {
         <ol className="list-decimal list-inside relative text-sm md:text-lg font-semibold mt-1">
             <li className='pl-4'><a className="font-extrabold dark:font-black">Python Plugin for Test Creation: </a>A robust backend built with Python that allows users to set up and configure audio tests efficiently. 
               It supports a range of test types such as simple question/rating, 
-              <a className='text-blue-400 no-underline hover:text-pink-500 hover:text-underline transform hover:scale-105 duration-300 ease-in-out' href="/about/ab"> AB</a>, 
-              <a className='text-blue-400 no-underline hover:text-pink-500 hover:text-underline transform hover:scale-105 duration-300 ease-in-out' href="/about/abx"> ABX</a>, 
-              <a className='text-blue-400 no-underline hover:text-pink-500 hover:text-underline transform hover:scale-105 duration-300 ease-in-out' href="/about/ape"> APE </a> and 
-              <a className='text-blue-400 no-underline hover:text-pink-500 hover:text-underline transform hover:scale-105 duration-300 ease-in-out' href="/about/mushra"> MUSHRA</a>, 
+              <a className='text-blue-400 dark:text-blue-500 no-underline hover:text-pink-500 dark:hover:text-pink-600 hover:text-underline transform hover:scale-105 duration-300 ease-in-out' href="/about/ab"> AB</a>, 
+              <a className='text-blue-400 dark:text-blue-500 no-underline hover:text-pink-500 dark:hover:text-pink-600 hover:text-underline transform hover:scale-105 duration-300 ease-in-out' href="/about/abx"> ABX</a>, 
+              <a className='text-blue-400 dark:text-blue-500 no-underline hover:text-pink-500 dark:hover:text-pink-600 hover:text-underline transform hover:scale-105 duration-300 ease-in-out' href="/about/ape"> APE </a> and 
+              <a className='text-blue-400 dark:text-blue-500 no-underline hover:text-pink-500 dark:hover:text-pink-600 hover:text-underline transform hover:scale-105 duration-300 ease-in-out' href="/about/mushra"> MUSHRA</a>, 
               catering to different research needs and scenarios.
             </li>
             <li className='pl-4 mt-2'><a className="font-extrabold dark:font-black">Web Application for Experimentation: </a>A user-friendly frontend developed using modern technologies like TypeScript and React.js, 
@@ -67,13 +94,23 @@ const About = (): JSX.Element => {
           your insights and contributions can help shape the future of audio quality assessment.
         </h3>
         <h3 className="relative text-sm md:text-lg font-semibold mt-4">
-          Explore more about PQToolkit and how you can contribute to this innovative project on <a className='text-blue-400 no-underline hover:text-pink-500 hover:text-underline transform hover:scale-105 duration-300 ease-in-out' href="https://github.com/pawelmuller/pq-toolkit">GitHub</a>.
+          Explore more about PQToolkit and how you can contribute to this innovative project on <a className='text-blue-400 dark:text-blue-500 no-underline hover:text-pink-500 dark:hover:text-pink-600 hover:text-underline transform hover:scale-105 duration-300 ease-in-out' href="https://github.com/pawelmuller/pq-toolkit">GitHub</a>.
         </h3>
         <h3 className="relative text-sm md:text-lg font-semibold mt-4">
           Thank you for your interest in the PQToolkit. Together, we can push the boundaries of audio quality research and development!
         </h3>
       </div>
     </div>
+    {isVisible && (
+        <div className="fixed bottom-5 right-5">
+          <button
+            onClick={scrollToTop}
+            className="p-3 rounded-full bg-blue-400 dark:bg-blue-500 text-white hover:bg-pink-500 dark:hover:bg-pink-600 transition-all duration-300"
+          >
+            <FaArrowUp className="w-6 h-6" />
+          </button>
+        </div>
+    )}
   </div>
   )
 }
