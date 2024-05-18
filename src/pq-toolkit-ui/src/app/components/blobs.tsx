fb8527bf2a42dbdc3484c9d34f6aede185b3ab5a
Nokerr21 <noker3009@gmail.com>
2024-05-18 11:39:50 +0200

    fix: sm screen size blobs adjust

--- src/pq-toolkit-ui/src/app/components/blobs.tsx
+++ src/pq-toolkit-ui/src/app/components/blobs.tsx
@@ -10,10 +10,13 @@ const Blobs = () => {
         <div className='absolute -top-14 lg:-top-28 -left-11 w-72 md:w-80 lg:w-96 h-72 md:h-80 lg:h-96 bg-none md:bg-gradient-to-r from-indigo-500 to-cyan-600 rounded-full mix-blend-multiply dark:mix-blend-color-dodge filter blur-xl opacity-50 animate-blob animation-delay-10000' />
 
 
-        <div className='absolute -top-14 -left-11 w-80 h-80 md:bg-none bg-gradient-to-r from-indigo-500 to-cyan-600 rounded-full mix-blend-multiply dark:mix-blend-color-dodge filter blur-xl opacity-50 animate-blob animation-delay-10000' />
-        <div className='absolute -top-10 left-36 w-64 h-64 md:bg-none bg-gradient-to-r from-cyan-600 to-pink-500 rounded-full mix-blend-multiply dark:mix-blend-color-dodge filter blur-xl opacity-60 animate-blob animation-delay-6000' />
-        <div className='absolute -top-10 -right-6 w-96 h-96 md:bg-none bg-gradient-to-r from-pink-500 to-violet-500 rounded-full mix-blend-multiply dark:mix-blend-color-dodge filter blur-xl opacity-60 dark:opacity-40 animate-blob animation-delay-8000' />
-        <div className='absolute top-16 right-32 w-80 h-80 md:bg-none bg-gradient-to-r from-pink-500 to-pink-700 rounded-full mix-blend-multiply dark:mix-blend-color-dodge filter blur-xl opacity-50 animate-blob animation-delay-4000' />
+        <div className='absolute -top-14 -left-11 w-80 h-80 md:bg-none min-[640px]:bg-gradient-to-r from-indigo-500 to-cyan-600 rounded-full mix-blend-multiply dark:mix-blend-color-dodge filter blur-xl opacity-50 animate-blob animation-delay-10000' />
+        <div className='absolute -top-10 left-36 w-64 h-64 md:bg-none min-[640px]:bg-gradient-to-r from-cyan-600 to-pink-500 rounded-full mix-blend-multiply dark:mix-blend-color-dodge filter blur-xl opacity-60 animate-blob animation-delay-6000' />
+        <div className='absolute -top-10 -right-6 w-96 h-96 md:bg-none min-[640px]:bg-gradient-to-r from-pink-500 to-violet-500 rounded-full mix-blend-multiply dark:mix-blend-color-dodge filter blur-xl opacity-60 dark:opacity-40 animate-blob animation-delay-8000' />
+        <div className='absolute top-16 right-32 w-80 h-80 md:bg-none min-[640px]:bg-gradient-to-r from-pink-500 to-pink-700 rounded-full mix-blend-multiply dark:mix-blend-color-dodge filter blur-xl opacity-50 animate-blob animation-delay-4000' />
+
+        <div className='absolute -top-16 -left-11 w-80 h-80 sm:bg-none bg-gradient-to-r from-indigo-500 to-cyan-600 rounded-full mix-blend-multiply dark:mix-blend-color-dodge filter blur-xl opacity-50 animate-blob animation-delay-2000' />
+        <div className='absolute -top-12 -right-6 w-96 h-96 sm:bg-none bg-gradient-to-r from-cyan-600 via-pink-700 to-violet-600 rounded-full mix-blend-multiply dark:mix-blend-color-dodge filter blur-xl opacity-60 dark:opacity-40 animate-blob animation-delay-4000' />
     </div>
   )
 }
