eda67c22fdbb84923f2e106a3cb64eeabcaa9f3f
Nokerr21 <noker3009@gmail.com>
2024-05-19 21:26:12 +0200

    change: UI of admin page - main

--- src/pq-toolkit-ui/src/app/components/admin-page.tsx
+++ src/pq-toolkit-ui/src/app/components/admin-page.tsx
@@ -66,7 +66,6 @@ const AdminPage = (props: any): JSX.Element => {
       .catch(console.error)
   }
 
-
   return (
     <div className="min-h-screen bg-gray-100 dark:bg-stone-900">
       <Header />
@@ -83,19 +82,18 @@ const AdminPage = (props: any): JSX.Element => {
           Logout
         </button>
       </div>
-      <div className="flex flex-col h-full w-full items-center justify-center my-auto mt-40">
-      <div className="relative text-center mb-sm">
-        <Blobs />
+      <div className="flex flex-col h-full w-full items-center justify-center my-auto mt-32">
+        <div className="relative text-center mb-sm md:mb-md lg:mb-lg">
+          <Blobs />
           <div className="fadeInUp">
             <h1 className="relative text-5xl md:text-6xl font-bold">Perceptual Qualities Toolkit</h1>
             <h2 className="relative text-2xl md:text-3xl font-semibold mt-sm">
               Experiment Configurator
             </h2>
+          </div>
         </div>
-      </div>
-        <div className='flex flex-row w-screen justify-between pr-10 pl-10 fadeInUp'>
+        <div className='flex flex-col w-full items-center fadeInUp'>
           {selectedExperiment == undefined ? <div /> : <CreateExperimentForm setSelectedExperiment={setSelectedExperiment} selectedExperiment={selectedExperiment} />}
-
           <AdminExperimentsListWidget
             experiments={data.experiments}
             deleteExperiment={deleteExperiment}
@@ -104,7 +102,7 @@ const AdminPage = (props: any): JSX.Element => {
           />
         </div>
       </div>
-  </div>
+    </div>
   )
 }
 
@@ -120,18 +118,18 @@ const AdminExperimentsListWidget = ({
   setSelectedExperiment: (name: any) => void
 }): JSX.Element => {
   return (
-    <div className="flex flex-col items-center z-10 self-end border-2 h-200 w-80 bg-white pr-10 pl-5 rounded-lg">
+    <div className="flex flex-col items-center z-10 w-full max-w-2xl text-white bg-white/10 dark:bg-gray-800/10 backdrop-blur-md rounded-3xl p-8 shadow-2xl">
+      <div className="flex text-lg md:text-xl font-semibold">Add Experiment</div>
       <AddExperimentWidget
         experiments={experiments}
         addExperiment={addNewExperiment}
       />
-      <div className="flex self-start mt-3 mb-2 text-black">Experiments:</div>
-      <div className='overflow-y-auto w-72'>
-        <ul>
+      <div className="flex self-start mt-3 mb-2 text-sm md:text-base font-semibold text-black dark:text-white">Experiments:</div>
+      <div className='overflow-y-auto w-full'>
+        <ul className="space-y-2 w-full">
           {experiments.map((name, idx) => (
-            <li key={idx} className="flex items-center gap-sm justify-between pl-4 z-10">
-              {/* <div onClick={() => setSelectedExperiment(name)}>{name}</div> */}
-              {<Link href={`/admin/${name}`} className="text-black">{name}</Link>}
+            <li key={idx} className="flex items-center gap-sm justify-between bg-blue-400 dark:bg-blue-500 hover:bg-pink-500 dark:hover:bg-pink-600 p-2 rounded-md whitespace-normal break-words">
+              <Link href={`/admin/${name}`} className="w-5/6 font-semibold">{name}</Link>
               <DeleteButton deleteExperiment={deleteExperiment} name={name} />
             </li>
           ))}
@@ -151,9 +149,9 @@ const AddExperimentWidget = ({
   const [newName, setNewName] = useState('')
 
   return (
-    <div className="flex items-center z-10 mt-4">
+    <div className="flex items-center z-10 mt-4 w-full">
       <input
-        className="rounded outline-0 border-2 bg-gray-200 text-black"
+        className="rounded outline-0 border-2 bg-gray-200 dark:bg-gray-300 dark:border-gray-300 text-black w-full"
         onChange={(e) => {
           setNewName(e.target.value)
         }}
@@ -165,7 +163,7 @@ const AddExperimentWidget = ({
           setNewName('')
         }}
         disabled={newName.length === 0 || experiments.includes(newName)}
-        className="flex items-center text-sm disabled:bg-gray-400 bg-blue-500 rounded-xl p-xxs ml-4"
+        className="flex items-center text-sm disabled:bg-gray-400 dark:disabled:bg-gray-400 bg-blue-400 dark:bg-blue-500 hover:bg-pink-500 dark:hover:bg-pink-600 transform hover:scale-105 duration-300 ease-in-out rounded-xl p-xxs ml-4 text-white"
       >
         <FaPlus />
       </button>
