fc877b0f55097dd601f9e10e82102b3e98e1bbd1
Jan Kowalczewski <official.jan.kk@gmail.com>
2024-05-21 15:08:28 +0200

    Uvicorn reloading in local development mode

--- src/pq-toolkit-api/main.py
+++ src/pq-toolkit-api/main.py
@@ -3,4 +3,7 @@ from app.core.config import settings
 import uvicorn
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=settings.PQ_API_PORT)
+    local_environment = settings.ENVIRONMENT == "local"
+    if local_environment:
+        print("Uvicorn reloading is enabled.")
+    uvicorn.run("app.main:app", host="0.0.0.0", port=settings.PQ_API_PORT, reload=local_environment)
