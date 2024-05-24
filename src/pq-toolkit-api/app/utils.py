233c08a87aa9720a48222bc11ef92daceaee499e
Igor Matynia <igor.matynia@gmail.com>
2024-05-24 21:34:18 +0200

    pqexception has custom error code

--- src/pq-toolkit-api/app/utils.py
+++ src/pq-toolkit-api/app/utils.py
@@ -4,6 +4,7 @@ from urllib3.response import HTTPResponse
 
 
 class PqException(Exception):
-    def __init__(self, message: str) -> None:
+    def __init__(self, message: str, error_code: int = 400) -> None:
         super().__init__(message)
         self.api_payload = PqErrorResponse(message=message)
+        self.error_code = error_code
