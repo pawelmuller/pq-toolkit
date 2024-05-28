1703dc74f33898fbe96e282f77f776f65401d32f
mikolajolczak <mikolaj.olczak12@gmail.com>
2024-05-29 00:49:30 +0200

    created tests deleteAxisComp, deletQuestionComp

--- src/pq-toolkit-ui/src/lib/components/form/deleteAxisComp.tsx
+++ src/pq-toolkit-ui/src/lib/components/form/deleteAxisComp.tsx
@@ -1,9 +1,6 @@
-import { useState } from "react";
+import React, { useState } from "react";
 import { FaCheck, FaTrash, FaTimes } from 'react-icons/fa';
-import {
-    type ABTest, type ABXTest, type FullABXTest, type MUSHRATest, type APETest, type BaseTest,
-    type Question
-} from '@/lib/schemas/experimentSetup'
+import { type ABTest, type ABXTest, type BaseTest, type FullABXTest, type MUSHRATest, type APETest, type Question } from '@/lib/schemas/experimentSetup';
 
 interface DeleteAxisCompProps {
     index: number
@@ -18,28 +15,43 @@ const DeleteAxisComp = (props: DeleteAxisCompProps): JSX.Element => {
         <div>
             {deleteConfirm ? (
                 <div className="flex flex-row mr-1">
-                    <FaTimes className="fill-red-500 mr-2 cursor-pointer transform hover:scale-125 duration-300 ease-in-out" size={25} onClick={() => { setDeleteConfirm(false) }} />
-                    <FaCheck className="fill-green-500 cursor-pointer transform hover:scale-125 duration-300 ease-in-out" size={24} onClick={() => {
-                        setDeleteConfirm(false)
-                        props.setCurrentTest((oldTest) => {
-                            if ('axis' in oldTest) {
-                                return {
-                                    ...oldTest,
-                                    axis: oldTest.axis.filter((_: Question, i: number) => i !== props.index)
+                    <FaTimes
+                        aria-label="cancel-delete"
+                        className="fill-red-500 mr-2 cursor-pointer transform hover:scale-125 duration-300 ease-in-out"
+                        size={25}
+                        onClick={() => { setDeleteConfirm(false); }}
+                    />
+                    <FaCheck
+                        aria-label="confirm-delete"
+                        className="fill-green-500 cursor-pointer transform hover:scale-125 duration-300 ease-in-out"
+                        size={24}
+                        onClick={() => {
+                            setDeleteConfirm(false);
+                            props.setCurrentTest((oldTest) => {
+                                if ('axis' in oldTest) {
+                                    return {
+                                        ...oldTest,
+                                        axis: oldTest.axis.filter((_: Question, i: number) => i !== props.index)
+                                    }
+                                } else {
+                                    return {
+                                        ...oldTest
+                                    }
                                 }
-                            } else {
-                                return {
-                                    ...oldTest
-                                }
-                            }
-                        })
-                    }} />
+                            });
+                        }}
+                    />
                 </div>
             ) : (
-                <FaTrash className="fill-red-500 cursor-pointer mr-0 sm:mr-2 transform hover:scale-125 duration-300 ease-in-out" size={25} onClick={() => { setDeleteConfirm(true) }} />
+                <FaTrash
+                    aria-label="delete-icon"
+                    className="fill-red-500 cursor-pointer mr-0 sm:mr-2 transform hover:scale-125 duration-300 ease-in-out"
+                    size={25}
+                    onClick={() => { setDeleteConfirm(true); }}
+                />
             )}
         </div>
     );
 };
 
-export default DeleteAxisComp
+export default DeleteAxisComp;
