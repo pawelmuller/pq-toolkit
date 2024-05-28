c7551f4ff1c0b0d39045d47274359c86a7f4d424
mikolajolczak <mikolaj.olczak12@gmail.com>
2024-05-28 21:29:46 +0200

    Updated SinglePlayer and Multiplayer tests

--- src/pq-toolkit-ui/src/lib/components/player/__tests__/MultiPlayer.test.tsx
+++ src/pq-toolkit-ui/src/lib/components/player/__tests__/MultiPlayer.test.tsx
@@ -1,9 +1,9 @@
 import '@testing-library/jest-dom'
-import { render, screen } from '@testing-library/react'
+import { render, screen, fireEvent } from '@testing-library/react'
 import MultiPlayer from '../MultiPlayer'
 
 describe('MultiPlayer', () => {
-  it('renders a single sample player', () => {
+  it('renders a multi sample player', () => {
     const [state, setState] = [0, jest.fn()]
     render(
       <MultiPlayer
@@ -17,11 +17,33 @@ describe('MultiPlayer', () => {
       />
     )
 
-    expect(screen.queryByTestId('play-button')).toBeInTheDocument()
-    expect(screen.queryByTestId('pause-button')).toBeInTheDocument()
-    expect(screen.queryByTestId('stop-button')).toBeInTheDocument()
+    expect(screen.queryByTestId('play-pause-button')).toBeInTheDocument()
+    expect(screen.queryByTestId('progress-slider')).toBeInTheDocument()
 
     expect(screen.getByText('sample1')).toBeInTheDocument()
     expect(screen.getByText('sample2')).toBeInTheDocument()
   })
+
+  it('toggles play and pause', () => {
+    const [state, setState] = [0, jest.fn()]
+    render(
+      <MultiPlayer
+        assets={
+          new Map<string, { url: string; footers?: JSX.Element[] }>([
+            ['sample1', { url: 'mock.mp3' }],
+            ['sample2', { url: 'mock2.mp3' }]
+          ])
+        }
+        selectedPlayerState={[state, setState]}
+      />
+    )
+
+    const playPauseButton = screen.getByTestId('play-pause-button')
+    fireEvent.click(playPauseButton)
+    expect(screen.queryByTestId('pause-icon')).toBeInTheDocument()
+
+    fireEvent.click(playPauseButton)
+    expect(screen.queryByTestId('play-icon')).toBeInTheDocument()
+  })
+
 })
